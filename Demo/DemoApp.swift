0ac62278bccad5b5bcdfdcd3c7a2c808902be44c
Luke Lai <lukelai.dev@gmail.com>
2023-05-08 15:11:02 +0100

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Luke Lai on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
